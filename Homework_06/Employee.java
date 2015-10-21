2e890ffb2dac57905cdb3abe2b51a05deb7ca064
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-10-20 20:50:14 -0400

    Homework 06: Switched to StringTokenizer

--- Homework_06/Employee.java
+++ Homework_06/Employee.java
@@ -2,6 +2,10 @@
 //2015-10-18
 //Homework 06
 
+//Needed for my compiler
+//  StringTokenizer is becoming obsolete
+import java.util.StringTokenizer;
+
 public class Employee {
   private String  strFirstName;
   private String  strLastName;
@@ -22,19 +26,48 @@ public class Employee {
   
   //Set Employee metadata from record string
   public void set(String strRecord) throws IllegalArgumentException {
-    if(!strRecord.matches("\\w+ \\w+ \\d+ \\d+ [\\d\\.]+ [TFtf]"))
+    //Check if the string is a valid database entry
+    if(!strRecord.matches("(\\w+\\s+){2}(\\d+\\s+){2}[\\d\\.]+\\s+[TF]"))
       throw new IllegalArgumentException("not a valid record: " + strRecord);
     
-    String[] astrFields = strRecord.split(" ");
+    //Tokenize the string
+    StringTokenizer st = new StringTokenizer(strRecord);
     
-    strFirstName = astrFields[0];
-    strLastName  = astrFields[1];
-    nID          = Integer.valueOf(astrFields[2]).intValue();
-    nAge         = Integer.valueOf(astrFields[3]).intValue();
-    dSalary      = Double .valueOf(astrFields[4]).doubleValue();
-    bFullTime    = astrFields[5].equalsIgnoreCase("T");
+    //Since line 26 checks if the string is valid, it would be wastfull to
+    //  have the computer check if st hasMoreTokens()
+    strFirstName = st.nextToken();
+    strLastName  = st.nextToken();
+    nID          = Integer.valueOf(st.nextToken()).intValue();
+    nAge         = Integer.valueOf(st.nextToken()).intValue();
+    dSalary      = Double .valueOf(st.nextToken()).doubleValue();
+    bFullTime    = st.nextToken().equalsIgnoreCase("T");
   }
   
+/** http://docs.oracle.com/javase/7/docs/api/java/util/StringTokenizer.html
+  *   StringTokenizer is a legacy class that is retained for compatibility
+  *     reasons although its use is discouraged in new code. It is recommended
+  *     that anyone seeking this functionality use the split method of String
+  *     or the java.util.regex package instead.
+  *
+  * //According to the abouve statment taken from oracle's Java API,
+  * //  this is what the set(String) method should look like
+  *
+  * //Set Employee metadata from record string
+  * public void set(String strRecord) throws IllegalArgumentException {
+  *   //Check if the string is a valid database entry
+  *   if(!strRecord.matches("(\\w+\\s+){2}(\\d+\\s+){2}[\\d\\.]+\\s+[TF]"))
+  *     throw new IllegalArgumentException("not a valid record: " + strRecord);
+  *   
+  *   String[] astrFields = strRecord.split("\t");
+  *   
+  *   strFirstName = astrFields[0];
+  *   strLastName  = astrFields[1];
+  *   nID          = Integer.valueOf(astrFields[2]).intValue();
+  *   nAge         = Integer.valueOf(astrFields[3]).intValue();
+  *   dSalary      = Double .valueOf(astrFields[4]).doubleValue();
+  *   bFullTime    = astrFields[5].equalsIgnoreCase("T");
+  * }
+  */
   
   //Accessor|Modifier methods - grouped by var used
   public void setFirstName(String strFirstName) {
@@ -87,7 +120,7 @@ public class Employee {
   
   //Used to store an Employee in a database
   public String toString() {
-    return String.format("%s %s %d %d %f %s",
+    return String.format("%s\t%s\t%d\t%d\t%f\t%s",
                           strFirstName,
                           strLastName,
                           nID,
