fe1c4e2cbd26b006d8efa5abc383b47e1edc8621
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-10-27 19:28:15 -0400

    Test 01: fixed an error

--- Test_01/FileStats.java
+++ Test_01/FileStats.java
@@ -1,17 +1,31 @@
-class FileStats 
-  FileInputStream 
-  public FileStats(String strFileName) {
-    File info = new File(strFileIn);
+import java.io.*;
+
+class FileStats {
+  public  final int      BLOCK_SIZE = 512;
+  
+  private FileReader     fr;
+  private BufferedReader br;
+  private String         strShortest;
+  private String         strLongest;
+  private String         strFileName;
+  private int            nChars;
+  private int            nWords;
+  
+  public FileStats(String strFileName) throws Exception {
+    this.strFileName = strFileName;
+    
+    //Check that file can be openned
+    File info = new File(this.strFileName);
     if(!info.exists()) {
-      System.out.printf("\"%s\" does not exist - Aborting\n", strFileIn);
+      System.out.printf("\"%s\" does not exist - Aborting\n", strFileName);
       System.exit(0);
     }
-    if(!info.isDirectory()) {
-      System.out.printf("\"%s\" is a directory - Aborting\n", strFileIn);
+    if(info.isDirectory()) {
+      System.out.printf("\"%s\" is a directory - Aborting\n", strFileName);
       System.exit(0);
     }
     if(!info.canRead()) {
-      System.out.printf("\"%s\" cannot be read - Aborting\n", strFileIn);
+      System.out.printf("\"%s\" cannot be read - Aborting\n", strFileName);
       System.exit(0);
     }
     
