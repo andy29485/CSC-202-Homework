9fac8ea979429ac3c0b5278f3f8e3875f0dce3a7
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-06 15:36:57 -0500

    Homework 07: Data base prints tables

--- Homework_07/Database.java
+++ Homework_07/Database.java
@@ -2,8 +2,32 @@
 //2015-11-06
 //Homework 07
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Database {
+  List<Entry> entries;
+  String filename;
+
   public Database() {
-  
+    this.entries  = new ArrayList<Entry>();
+    this.filename = "";
+  }
+
+  public Database(String filename) {
+    this.entries  = new ArrayList<Entry>();
+    this.filename = filename;
+  }
+
+  public void print() {
+    System.out.println("    L    |    C    |   Cmin  |   Cmax  ");
+    System.out.println("---------------------------------------");
+    for(Entry entry : entries) {
+      System.out.printf("%8f |%8f |%8f |%8f\n",
+                        entry.getL(),
+                        entry.getC(),
+                        entry.getCmin(),
+                        entry.getCmax());
+    }
   }
 }
