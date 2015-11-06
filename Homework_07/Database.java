95d1f8754ff77e4b673797b5d25efa9c9f599589
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-06 14:46:44 -0500

    Homework 07: start

---
+++ Homework_07/Database.java
@@ -0,0 +1,9 @@
+//Andriy Zasypkin
+//2015-11-06
+//Homework 07
+
+public class Database {
+  public Database() {
+  
+  }
+}
---
+++ Homework_07/Main.java
@@ -0,0 +1,19 @@
+//Andriy Zasypkin
+//2015-11-06
+//Homework 07
+
+public class Main {
+  public static void main(String[] args) {
+    //create an input reader object
+    BufferedReader input
+            = new BufferedReader(new InputStreamReader(System.in));
+    
+    
+    
+    //close stream
+    input.close();
+    
+    System.exit(0);
+  }
+}
+
