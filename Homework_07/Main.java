8eb83c7c7484ecbe0f02e94dd2afe856cf11639f
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-06 23:05:49 -0500

    Homework 07: Added comments

--- Homework_07/Main.java
+++ Homework_07/Main.java
@@ -4,6 +4,16 @@
 
 import java.io.*;
 
+/** Description:
+  *   Create a program to record status(?) of a tuning circut
+  *     - For frequencies in range [Fmin, Fmax] #TODO - fix
+  *     - Use a random acces file
+  *        * also save Fmin and Fmax #TODO
+  *
+  * Assumptions:
+  *   Movies and movie metadata can be hard coded
+  */
+
 public class Main {
   public static void main(String[] args) throws IOException {
     //create an input reader object
@@ -29,7 +39,10 @@ public class Main {
       }
     } while(info.isDirectory());
 
+    //create database and set the filename for database
     Database db = new Database(info.getPath());
+    
+    //Load the database form file(if file exists)
     if(info.exists())
       db.load();
 
@@ -40,7 +53,7 @@ public class Main {
     // default values:
     //   frequency(MHz):                 16.7
     //   increment(pF):                  30
-    //   mi/imal capacitance value(pF):  15
+    //   minimal capacitance value(pF):  15
     //   maximum capacitance value(pF): 365
 
     System.out.print("Enter a frequency(MHz): "); //Multiplyed to get to base
@@ -48,32 +61,37 @@ public class Main {
     System.out.print("Enter an increment(pF): ");
     double inc = Double.valueOf(input.readLine()).doubleValue();
     System.out.print("Enter a minimal capacitance value(pF): ");
-    double cmn = Double.valueOf(input.readLine()).doubleValue();
+    double fn = Double.valueOf(input.readLine()).doubleValue();
     System.out.print("Enter a maximum capacitance value(pF): ");
-    double cmx = Double.valueOf(input.readLine()).doubleValue();
+    double fx = Double.valueOf(input.readLine()).doubleValue();
 
     double c = Math.sqrt(cmn*cmx);
     double l = Math.sqrt(Math.pow((2*Math.PI/f), 2)/c);
 
-    for(double i=cmn; i<=cmx; i+=inc) {
+    for(double i=cmn; i<=cmx; i+=inc) { #TODO - fix(see line 9ish)
       db.add(l, cmn, i);
     }
     /*/
 
+    //INPUT:
+    //  ask user which entry they want to modify
     System.out.printf("\nEntry to modify[1-%d]: ", db.size());
     int index = Integer.valueOf(input.readLine()).intValue() - 1;
 
-    System.out.printf("New value for cmax of entry %d in Farads: ", index);
+    //INPUT:
+    //  get the value of Cmax of entry user wants to modify from the user
+    System.out.printf("New value for cmax of entry %d in pF: ", index);
     double new_cmax = Double.valueOf(input.readLine()).doubleValue();
 
+    //Set the value of Cmax of entry user wants to modify, and re-calc 'C'
     db.modify(index).setCmax(new_cmax);
     db.modify(index).calcC();
 
     System.out.println("\nModified database:");
     db.print();//Print database after the modification */
 
-    //db.save(index);//save only the modified entry
-    db.save();   //Save entire database
+    db.save(index);//save only the modified entry
+    //db.save();   //Save entire database
 
     //close input stream
     input.close();
