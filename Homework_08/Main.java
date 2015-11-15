cb1e31df45db1c1124e4a11094b844b3adcd1fed
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-14 19:36:14 -0500

    Homework 08: Finished main

--- Homework_08/Main.java
+++ Homework_08/Main.java
@@ -18,11 +18,48 @@ public class  {
             = new BufferedReader(new InputStreamReader(System.in));
     
     //read 3 matrixes
-    System.out.print("Rows in matrix A: ");
+    System.out.print("Rows in matrix A: ");//get rows
     int rows = Integer.valueOf(input.readLine()).intValue();
-    System.out.print("Columns in matrix A: ");
+    System.out.print("Columns in matrix A: ");//get columns
     int cols = Integer.valueOf(input.readLine()).intValue();
-    Matrix A = new Matrix(rows, cols);
+    Matrix A = new Matrix(rows, cols);//and create
+    
+    System.out.print("Rows in matrix B: ");//get rows
+    int rows = Integer.valueOf(input.readLine()).intValue();
+    System.out.print("Columns in matrix B: ");//get columns
+    int cols = Integer.valueOf(input.readLine()).intValue();
+    Matrix B = new Matrix(rows, cols);//and create
+    
+    System.out.print("Rows in matrix C: ");//get rows
+    int rows = Integer.valueOf(input.readLine()).intValue();
+    System.out.print("Columns in matrix C: ");//get columns
+    int cols = Integer.valueOf(input.readLine()).intValue();
+    Matrix C = new Matrix(rows, cols);//and create
+    
+    //D = A + B
+    Matrix D = A.add(B);
+    
+    //E = A + C - B
+    Matrix E = A.add(C).subtract(B);
+    
+    //E * k
+    System.out.print("Enter a constant: ");
+    Matrix Ek = E.multiply(Integer.valueOf(input.readLine()).intValue());
+    
+    //OUTPUT:
+    //  print all matrixes
+    System.out.println("A:");
+    A.print();
+    System.out.println("B:");
+    B.print();
+    System.out.println("C:");
+    C.print();
+    System.out.println("D:");
+    D.print();
+    System.out.println("E:");
+    E.print();
+    System.out.println("E*k:");
+    Ek.print();
     
     //close stream
     input.close();
