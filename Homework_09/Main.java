c5bb6d11e2014b8c5224cbcfc68c42aa283abd46
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-18 17:08:47 -0500

    Homework 09: description was writen

---
+++ Homework_09/Main.java
@@ -0,0 +1,26 @@
+//Andriy Zasypkin
+//2015-11-18
+//Homework 09
+
+import java.io.*;
+
+/** Description:
+  *   Write a program in java to simulate the behaviour of an elevator
+  *     - starts at first(1) floor
+  *     - goes to first floor if no message
+  *     - only authorized users can go to top floor(26)
+  *     - 25 story building
+  *     - no floor 13
+  *     - door has a delay of 1 min
+  *       * button may be pressed to extend time
+  *
+  * Assumptions:
+  *   - 
+  */
+
+public class Main {
+  public static void main(String[] args) {
+    
+    System.exit(0);
+  }
+}
