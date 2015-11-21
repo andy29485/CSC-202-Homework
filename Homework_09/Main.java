8bd3d73a16f7579a5e0f1c4a05ad050a767fa28c
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-20 20:42:36 -0500

    Homework 09: Finished main
    not that I did much,
    although I guess assumption count...

--- Homework_09/Main.java
+++ Homework_09/Main.java
@@ -18,10 +18,32 @@ import java.io.*;
   *   - print out every time elevator does something
   *     * move up/down a floor
   *     * open/close door
+  *     * button to keep door waiting, will output waiting
+  *   - times are actual times
+  *   - input done via command line args
+  *   - will not move to first floor unless not active
+  *     * timeout will also be a minute
+  *     * will print that timeout was reached when it is reached, before going
+  *         down
+  *   - input will be a number(1-26) to move to that floor
+  *     * or 'o' to keep doors open
+  *   - queueing will not be implemented
+  *     * elevator will not move up/down, unless current move is finished
+  *     * guests are encouraged to keep poundng on the button
+  *         until the elevator opens its doors on their floor
+  *   - password will be needed to go to the top floor
+  *     * this will not have a timeout
+  *     * in honour of king roland of Druidia, the combination will be 12345
+  *     * wrong combination means that they will need to request the
+  *         top floor again
   */
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws java.io.IOException {
+    
+    Elevator elevator = new Elevator();
+    
+    elevator.start();
     
     System.exit(0);
   }
