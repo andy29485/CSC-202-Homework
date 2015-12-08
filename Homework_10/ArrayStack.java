3a72edeb7876a6ea092fe13700289f48c79610ac
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-12-08 18:15:53 -0500

    Homework 10: Stacks have TODOs

--- Homework_10/ArrayStack.java
+++ Homework_10/ArrayStack.java
@@ -3,7 +3,33 @@
 //az
 
 public class ArrayStack {
+  private int[] nums;
+  private int   topp;
+
   public ArrayStack() {
+    this.topp = -1;
+    this.nums = new int[5];
+  }
+
+  public boolean empty() {
+    return this.topp < 0;
+  }
+
+  public void push(int value) {
     //TODO
   }
+
+  public int pop() {
+    //TODO
+  }
+
+  public int peek() {
+    //TODO
+  }
+
+  /*No need - main only excepts 5 digits
+   * public boolean full() {
+   *   return this.topp == this.nums.length-1;
+   * }
+   */
 }
--- Homework_10/LinkStack.java
+++ Homework_10/LinkStack.java
@@ -3,7 +3,32 @@
 //Homework 10
 
 public class LinkStack {
+  private Digit topp;
+
   public LinkStack() {
+    top = null;
+  }
+
+  public boolean empty() {
+    return this.topp == null;
+  }
+
+  public void push(int value) {
     //TODO
   }
+
+  public int pop() {
+    //TODO
+  }
+
+  public int peek() {
+    //TODO
+  }
+
+  /*No need - main only excepts 5 digits
+   * public boolean full() {
+   *   //No implementation for linked list stacks
+   *   return false;
+   * }
+   */
 }
