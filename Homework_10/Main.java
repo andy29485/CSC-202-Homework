0f008688736ebf25c10f4eec52c168019962e9c5
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-12-08 18:22:20 -0500

    Homework 10: Completed Main

--- Homework_10/Main.java
+++ Homework_10/Main.java
@@ -4,7 +4,38 @@
 
 public class Main {
   public static void main(String[] args) {
-    //TODO test
+    //create an input reader object
+    BufferedReader input
+            = new BufferedReader(new InputStreamReader(System.in));
+
+    ArrayStack as = new ArrayStack();
+    LinkStack  ls = new LinkStack();
+    String     num;
+
+    System.out.print("Enter a 5 digit number for the ArrayStack: ");
+    num = input.readLine();
+    for(int i=0; i<5; i++) {
+      int tmp_digit = Integer.valueOf(num.charAt(i)).intValue();
+      as.push(tmp_digit);
+    }
+    System.out.println("Popping digits from ArrayStack:");
+    while(!as.empty()) {
+      System.out.print(as.pop());
+    }
+
+    System.out.print("\n\nEnter a 5 digit number for the LinkStack: ");
+    num = input.readLine();
+    for(int i=0; i<5; i++) {
+      int tmp_digit = Integer.valueOf(num.charAt(i)).intValue();
+      ls.push(tmp_digit);
+    }
+    System.out.println("Popping digits from ArrayStack:");
+    while(!ls.empty()) {
+      System.out.print(ls.pop());
+    }
+
+    //close stream
+    input.close();
     System.exit(0);
   }
 }
