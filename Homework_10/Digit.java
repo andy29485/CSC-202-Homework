b04fcf377e9107134805b9b56f3ad8bf652909a6
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-12-08 18:08:18 -0500

    Homework 10: Digit implemented

--- Homework_10/Digit.java
+++ Homework_10/Digit.java
@@ -3,11 +3,43 @@
 //Homework 10
 
 public class Digit {
-  public Digit() {
+  private int   value;
+  private Digit next;
+  private Digit prev;
 
+  public Digit() {
+    this.value = 0;
+    this.next  = null;
+    this.prev  = null;
+  }
+
+  public Digit(int value) {
+    this.value = value;
+    this.next  = null;
+    this.prev  = null;
+  }
+
+  public void setNext(Digit next) {
+    this.next = next;
   }
 
   public Digit getNext() {
-    //TODO - return next link
+    return this.next;
+  }
+
+  public void setPrev(Digit prev) {
+    this.prev = prev;
+  }
+
+  public Digit getPrev() {
+    return this.prev;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return this.value;
   }
 }
